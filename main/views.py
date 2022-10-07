1a468780d946adc28da20dd3587f9fe23d38bbe9
Saad-vavat <74826253+Saad-vavat@users.noreply.github.com>
2022-10-07 06:33:00 +0530

    Update views.py

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render,redirect
 
 from django.contrib import messages
 from django.contrib.auth import authenticate, login , logout
-from .forms import CreateUserForm
+
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.

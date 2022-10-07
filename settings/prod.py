4acaa71d3f0a90cf48e477247489d3d38f1991fb
Saad-vavat <74826253+Saad-vavat@users.noreply.github.com>
2022-10-07 07:12:21 +0530

    Add files via upload

---
+++ settings/prod.py
@@ -0,0 +1,7 @@
+# prod.py
+# Production settings for myapp
+from . import *  # Import base settings from settings/__init__.py
+
+ALLOWED_HOSTS = ["*"]
+DEBUG = False
+# ... whatever else you need ...
